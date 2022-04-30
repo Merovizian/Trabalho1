8966d21bde02392662734bf41b86d8919fa8f9d5
Merovizian <48018611+Merovizian@users.noreply.github.com>
2022-04-29 23:38:12 -0300

    Versao 2

--- SocketA.py
+++ SocketA.py
@@ -36,7 +36,9 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST = (input("Por favor informe o endereço da Maquina G1 [enter = 192.168.128.2]: "))
 if HOST == '':
     HOST = '192.168.128.2'
-PORT = int(input("Informe o numero do PORT CRIADO na Maquina G1: "))
+PORT = (input("Informe o numero do PORT CRIADO na Maquina G1: "))
+if PORT == '':
+    PORT = 5800
 
 while falha:
 
