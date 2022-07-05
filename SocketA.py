4213f9d282909665156caee1ed43a3bb3ec51f7f
Merovizian <ericgmicaela@gmail.com>
2022-07-05 08:55:49 -0300

    Retorno a versão anterior

--- SocketA.py
+++ SocketA.py
@@ -49,103 +49,82 @@ else:
 
 while falha:
 
+    try:
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print(f"Tentando conectar ao endereço '{HOST}' pela porta {PORT}")
+        tcp.connect((HOST, PORT))
+        ''' VARIAVEIS:  '''
+        quantidade = int(input("Qual a quantidade de matrizes: "))  # Pergunta ao usuário qual a quantidade de matrizes
+        ordem = int(input("Informe a ordem da matriz: "))  # Coloca na variavel ordem um inteiro que sera o tamanho da matriz
+        # texto = list()  # lista para registar em arquivo as matrizes geradas.
 
+        # Variavel que registra o tempo atual em segundos para calculo do tempo total gasto
+        ini_total = time.time()
 
+        # Função para geração dos valores e registro em uma lista os valores dos elementos em seus respectivos indices
+        arquivo = matrizmake(quantidade, ordem)
 
-    # texto = list()  # lista para registar em arquivo as matrizes geradas.
+        # Variavel que registra o tempo atual em segundos para calculo do tempo total gasto
+        fim_total = time.time()
 
-    # Variavel que registra o tempo atual em segundos para calculo do tempo total gasto
-    ini_total = time.time()
+        m = arquivo[0]['quantidade']
+        n = arquivo[0]['ordem']
 
+        b = json.dumps(arquivo).encode('utf-8')
+        tcp.sendall(b)
+        tcp.close()
 
+        # Utiliza as variaveis para calcular o tempo passsado para gerar cada matriz
+        print(f"\033[1;32mForam geradas {quantidade} matrizes em {fim_total - ini_total:.5f} segundos\033[m")
 
-    # Variavel que registra o tempo atual em segundos para calculo do tempo total gasto
-    fim_total = time.time()
+        # IMPRIME AS MATRIZES
+        resultado = input("Deseja imprimir as matrizes? ")
+        while resultado not in ("N, Nao, Sim, S, NAO, SIM, s,n"):
+            resultado = input("Opção inválida, por favor digite [N/S]: ")
+        print()
+        m = arquivo[0]['quantidade']
+        n = arquivo[0]['ordem']
+        print(f"Foram recebidas {m} Matrizes de Ordem {n} x {n}")
+        auxiliar = list()
+        auxiliar2 = list()
+        for b in range(0, arquivo[0]['quantidade']):
+            for a in arquivo[b]['matriz']:
+                if a == '\n':
+                    auxiliar2.append(auxiliar.copy())
+                    auxiliar.clear()
+                if a in ('0,1,2,3,4,5,6,7,8,9'):
+                    auxiliar.append(int(a))
+            arquivo[b]['matriz'] = auxiliar2.copy()
+            auxiliar2.clear()
 
+        if resultado in ("sim, S, SIM,s"):
+            for a in range(0, m):
+                print(f"Matriz {a + 1}")
+                for b in range(0, n):
+                    print(arquivo[a]['matriz'][b])
+        opcao = input("Deseja mandar outros arquivos? [S/N] ")
+        while opcao not in ("N, Nao, Sim, S, NAO, SIM, s,n"):
+            opcao = input("Opção inválida, por favor digite [N/S]: ")
+        if opcao in ("sim, S, SIM,s"):
+            falha = 1
+        else:
+            falha = 0
 
-    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print(f"Tentando conectar ao endereço '{HOST}' pela porta {PORT}")
-    tcp.connect((HOST, PORT))
-    # Função para geração dos valores e registro em uma lista os valores dos elementos em seus respectivos indices
-    ''' VARIAVEIS:  '''
-    quantidade = int(input("Qual a quantidade de matrizes: "))  # Pergunta ao usuário qual a quantidade de matrizes
-    ordem = int(input("Informe a ordem da matriz: "))  # Coloca na variavel ordem um inteiro que sera o tamanho da matriz
-    arquivo = matrizmake(quantidade, ordem)
-    m = (arquivo[0]['quantidade'])
-    print(m)
-    tcp.sendfile(arquivo)
-    print(f"Quantidade {m} de matrizes enviadas")
-    n = str(arquivo[0]['ordem'])
-    print(f"Ordem {n} das matrizes enviada")
-    tcp.send(n)
+    except:
+        tcp.close()
+        print(f"Endereço '{HOST}' não conectado!!")
+        HOST = (input("Por favor informe o endereço da Maquina G1 [enter = 192.168.128.2]: "))
+        if HOST == '':
+            HOST = '192.168.128.2'
+        PORT = (input("Informe o numero do PORT CRIADO na Maquina G1 [enter = 5800]: "))
+        if PORT == '':
+            PORT = 5800
+        else:
+            PORT = int(PORT)
 
-
-    #INICIO TESTE PARA ENVIAR POR STRINGS
-
-    '''        for x in range(0, quantidade):
-        tcp.send()    
-        
-        
-    for y in range(0, ordem):
-            numero = random.randint(0, 9)
-            texto.append(f'{numero:0>{len(str(9))}}') 
-    '''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    '''
-    b = json.dumps(arquivo).encode('utf-8')
-    tcp.sendall(b)
-    tcp.close()'''
-
-    # Utiliza as variaveis para calcular o tempo passsado para gerar cada matriz
-    print(f"\033[1;32mForam geradas {quantidade} matrizes em {fim_total - ini_total:.5f} segundos\033[m")
-
-    # IMPRIME AS MATRIZES
-    resultado = input("Deseja imprimir as matrizes? ")
-    while resultado not in ("N, Nao, Sim, S, NAO, SIM, s,n"):
-        resultado = input("Opção inválida, por favor digite [N/S]: ")
-    print()
-    m = arquivo[0]['quantidade']
-    n = arquivo[0]['ordem']
-    print(f"Foram recebidas {m} Matrizes de Ordem {n} x {n}")
-    auxiliar = list()
-    auxiliar2 = list()
-    for b in range(0, arquivo[0]['quantidade']):
-        for a in arquivo[b]['matriz']:
-            if a == '\n':
-                auxiliar2.append(auxiliar.copy())
-                auxiliar.clear()
-            if a in ('0,1,2,3,4,5,6,7,8,9'):
-                auxiliar.append(int(a))
-        arquivo[b]['matriz'] = auxiliar2.copy()
-        auxiliar2.clear()
-
-    if resultado in ("sim, S, SIM,s"):
-        for a in range(0, m):
-            print(f"Matriz {a + 1}")
-            for b in range(0, n):
-                print(arquivo[a]['matriz'][b])
-    opcao = input("Deseja mandar outros arquivos? [S/N] ")
-    while opcao not in ("N, Nao, Sim, S, NAO, SIM, s,n"):
-        opcao = input("Opção inválida, por favor digite [N/S]: ")
-    if opcao in ("sim, S, SIM,s"):
         falha = 1
     else:
-        falha = 0
-
-
+        print()
 
 print("ALEX, CONTO COM SUA MÃOZINHA LEVE")
 print()
