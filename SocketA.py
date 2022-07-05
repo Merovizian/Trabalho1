9a45036c1ae01a2e0bfe677e8d75fd4ddc72ced0
Merovizian <ericgmicaela@gmail.com>
2022-07-05 09:34:22 -0300

    TENTANDO VERIFICAR O PROBLEMA COM A SOCKET C V0.4

--- SocketA.py
+++ SocketA.py
@@ -28,6 +28,7 @@ def matrizmake(quantidade, ordem):
         matriz['matriz'] = texto.copy()
         texto.clear()
         print(f"\033[1;34mA MATRIZ {contador + 1} foi gerada com {time.time()-matriz['inicial']:.5f} segundos.... \033[m")
+        time.sleep(0.05)
         listamatriz.append(matriz.copy())
 
     return listamatriz
